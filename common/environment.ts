1f470167ba750bb69cf71cb428006e0c3dfcbed0
Cesar Melo <cavmelo@gmail.com>
2019-11-14 03:02:53 -0400

    setup mongodb from atlas

--- common/environment.ts
+++ common/environment.ts
@@ -1,6 +1,6 @@
 export const environment = {
   server:{port: process.env.SERVER_PORT || 3000},
-  db:{url:process.env.DB_URL||'mongodb://logger:01Logger10@logging-shard-00-00-psbis.gcp.mongodb.net:27017/logs-universe?ssl=true&authSource=admin&replicaSet=logging-shard-0'},
+  db:{url:process.env.DB_URL||'mongodb://logger:RmIFvQG2G5BPR6TM@logging-shard-00-00-psbis.gcp.mongodb.net:27017/logs-universe?ssl=true&authSource=admin&replicaSet=logging-shard-0'},
   //db:{url:process.env.DB_URL||'mongodb://logging-shard-00-00-psbis.gcp.mongodb.net/logs-universe'},
   //db:{url:process.env.DB_URL||'mongodb://localhost/logs-universe'},
   security:{saltRounds:10}
